28a8e5930ab3d4f07bac09dee056922fd56fd297
sameer-khalili <sameer.khalili@student.hu.nl>
2020-06-12 15:02:33 +0200

    git pipeline added

---
+++ src/test/java/hu/backend/lingo/application/findWordByIDTest.java
@@ -0,0 +1,26 @@
+package hu.backend.lingo.application;
+
+import hu.backend.lingo.domain.Word;
+import org.eclipse.collections.api.bag.MutableBag;
+import org.junit.Test;
+import org.junit.jupiter.api.DisplayName;
+
+import static org.junit.Assert.*;
+
+import java.util.Optional;
+
+public class findWordByIDTest {
+
+
+//    @Test
+//    @DisplayName("bag Result")
+//    public void makeWordBag (){
+//        GameService gameService = new GameService();
+//        MutableBag<Character> bag = gameService.bagOfLetters("Hallo");
+//        assertEquals(3, bag.occurrencesOf("l"));
+//
+//    }
+
+
+
+}
